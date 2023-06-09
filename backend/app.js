df79a1bde2f03336693e06a45e41041c54e5f10b
Sanjoy9800 <sanjurudra4@gmail.com>
2023-06-09 12:03:19 +0530

    first commit

---
+++ backend/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router=require("./routes/book-routes")
+const cors = require("cors");
+const app = express();
+
+// //middlewares
+
+app.use(express.json());
+app.use(cors());
+app.use("/books",router)
+
+mongoose
+  .connect(
+    "mongodb+srv://codemas:drhkzyGgwCeOWwsX@cluster0.18rh2ym.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("Connected To Database"))
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((err) => console.log(err));
+
+
+// drhkzyGgwCeOWwsX
