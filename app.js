24635e6d8b340997543503fef836593db0930145
Sanjoy9800 <sanjurudra4@gmail.com>
2023-06-10 19:32:20 +0530

    changes

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const router=require("./routes/book-routes")
+const router = require("./routes/book-routes");
 const cors = require("cors");
 const app = express();
-const port = process.env.port || 5000
+const port = process.env.port || 5000;
 
 // //middlewares
 
 app.use(express.json());
 app.use(cors());
-app.use("/books",router)
+app.use("/books", router);
 
 mongoose
   .connect(
@@ -17,11 +17,10 @@ mongoose
   )
   .then(() => console.log("Connected To Database"))
   .then(() => {
-    app.listen(port,()=>{
-      console.log(`Server start as port no ${port}`)
+    app.listen(port, () => {
+      console.log(`Server start as port no ${port}`);
     });
   })
   .catch((err) => console.log(err));
-
 
 // drhkzyGgwCeOWwsX
